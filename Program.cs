f8d1017e8e23c7dfba5fd929f5ab6b801b836995
Katrina Fuller <trinafuller@att.net>
2019-10-02 20:19:55 -0600

    mvp

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace guess
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Clear();
+      Random random = new Random();
+      int randomNumber = random.Next(1, 101);
+      bool won = false;
+      while (!won)
+      {
+        Console.WriteLine("Guess a number between 1 - 100");
+        string guess = Console.ReadLine();
+        int num;
+        if (Int32.TryParse(guess, out num))
+        {
+          if (num == randomNumber)
+          {
+            Console.Clear();
+            Console.WriteLine("You Win!");
+            won = true;
+          }
+          else if (num < randomNumber)
+          {
+            Console.Clear();
+            Console.WriteLine("Nope too low, guess higher");
+          }
+          else
+          {
+            Console.Clear();
+            Console.WriteLine("Nope too high, guess lower");
+          }
+        }
+
+      }
+      Console.WriteLine("Thank you for playing!");
+
+
+    }
+  }
+}
